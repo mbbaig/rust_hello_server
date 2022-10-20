0d31e71d8090425c304ddf93abb25478c911c7a9
Mohamed Baig <mohamed.b.baig@gmail.com>
2022-10-20 18:31:21 -0400

    feat: wip templating lib

---
+++ src/base.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, web, Responder};
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct MyObj {
+    hello: String
+}
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    web::Json(MyObj {
+        hello: "world".to_string()
+    })
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+mod base;
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
-}
+use actix_web::{App, HttpServer};
+use base::greet;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
     })
     .bind(("127.0.0.1", 8080))?
